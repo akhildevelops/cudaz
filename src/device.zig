88e64a7414002c739327e1a4ae704b78c663f1f9
akhildevelops <akhilgodvsdemon@gmail.com>
2023-11-14 10:55:50 +0530

    rename cuda_device to device

--- src/device.zig
+++ src/device.zig
@@ -4,29 +4,8 @@ const testing = std.testing;
 const cuda = @import("cuda.zig");
 const Error = @import("error.zig");
 const CudaError = Error.CudaError;
-// fn someCuda() !void {
-//     var device: i32 = 10;
-//     var dummy: u64 = 10;
-//     var context: cuda.CUcontext = undefined;
-//     std.debug.print("{s}\n", .{"start"});
-//     try Error.fromCudaErrorCode(cuda.cuInit(0));
-//     std.debug.print("{s}\n", .{"init"});
-//     try Error.fromCudaErrorCode(cuda.cuDeviceGet(&device, 0));
-//     std.debug.print("{s}\n", .{"get_device"});
-//     try Error.fromCudaErrorCode(cuda.cuDevicePrimaryCtxRetain(&context, device));
-//     std.debug.print("{s}\n", .{"get_context"});
-//     try Error.fromCudaErrorCode(cuda.cuCtxSetCurrent(context));
-//     std.debug.print("{s}\n", .{"set_context"});
-//     try Error.fromCudaErrorCode(cuda.cuMemAlloc_v2(&dummy, 1024 * 1024 * 5));
-//     std.debug.print("{s}\n", .{"allocated"});
-//     std.time.sleep(10 * std.time.ns_per_s);
-// }
 
-// test "basic add functionality" {
-//     try testing.expectError(error.CUDA_ERROR_INVALID_VALUE, someCuda());
-// }
-
-pub const CudaDevice = struct {
+pub const Device = struct {
     device: cuda.CUdevice,
     primary_context: cuda.CUcontext,
     ordinal: u16,
@@ -126,7 +105,7 @@ fn CudaSlice(comptime T: type) type {
     return struct {
         device_ptr: cuda.CUdeviceptr,
         len: usize,
-        device: CudaDevice,
+        device: Device,
         pub const element_type: type = T;
         pub fn clone(self: @This()) CudaError.Error!@This() {
             const c_slice = try self.device.alloc(T, self.len);
--- test.zig
+++ test.zig
@@ -1,27 +1,27 @@
-const all_cuda = @import("src/lib.zig");
+const Cuda = @import("src/lib.zig");
 const std = @import("std");
 const path = @import("src/path.zig");
 test "Setup" {
-    const device = try all_cuda.CudaDevice.default();
+    const device = try Cuda.Device.default();
     defer device.free();
 }
 
 test "Allocate" {
-    const device = try all_cuda.CudaDevice.default();
+    const device = try Cuda.Device.default();
     defer device.free();
     const slice = try device.alloc(f32, 1024 * 1024 * 1024);
     defer slice.free();
 }
 
 test "host_to_device" {
-    const device = try all_cuda.CudaDevice.default();
+    const device = try Cuda.Device.default();
     defer device.free();
     const slice = try device.htod_copy(f32, &[_]f32{ 1.2, 3.4, 8.9 });
     defer slice.free();
 }
 
 test "device_to_host" {
-    const device = try all_cuda.CudaDevice.default();
+    const device = try Cuda.Device.default();
     defer device.free();
     var float_arr = [_]f32{ 1.2, 3.4, 8.9 };
     const slice = try device.htod_copy(f32, &float_arr);
@@ -33,7 +33,7 @@ test "device_to_host" {
 // Got segmentation fault because n was declared a  comptime_int construct and after compiling there will be no n, therefore cuda wan't able to fetch the value n.
 // Even if n is declared as const param there's seg fault, unless it's declared as var
 test "ptx_file" {
-    var device = try all_cuda.CudaDevice.default();
+    var device = try Cuda.Device.default();
     defer device.free();
     var name = [_][]const u8{
         "sin_kernel",
@@ -44,7 +44,7 @@ test "ptx_file" {
     var float_arr = [_]f32{ 1.57, 0.5236, 0, 1.05 };
     const src_slice = try device.htod_copy(f32, &float_arr);
     var dest_slice = try src_slice.clone();
-    const cfg = all_cuda.LaunchConfig.for_num_elems(float_arr.len);
+    const cfg = Cuda.LaunchConfig.for_num_elems(float_arr.len);
     var n: i32 = float_arr.len;
     std.debug.print("{d}\n", .{@sizeOf(@TypeOf(n))});
     try device.launch_func(func, cfg, .{ &dest_slice.device_ptr, &src_slice.device_ptr, &n });
