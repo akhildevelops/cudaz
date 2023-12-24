0f7dc54f0ba7bce9432e3a277eb9a95c0dc6951b
akhildevelops <akhilgodvsdemon@gmail.com>
2023-12-24 22:34:04 +0530

    export error

--- src/lib.zig
+++ src/lib.zig
@@ -2,3 +2,6 @@ pub const Device = @import("device.zig");
 pub const Compile = @import("compile.zig");
 pub const LaunchConfig = @import("launchconfig.zig").LaunchConfig;
 pub usingnamespace @import("path.zig");
+
+const Error = @import("error.zig");
+pub const CudaError = Error.CudaError || Error.NvrtcError;
